f82674c9e2f6184748e84294ebd43e6da2b5931c
Devasheesh Upreti <devasheesh.upreti@gmail.com>
2025-08-12 17:15:27 +0530

    initial commit

---
+++ Portfolio/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+		<div class="area object-contain overflow-auto pb-5">
+			<ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+      <App className='m-0 p-0 flex flex-col w-full'/>
+    </div>
+  </StrictMode>,
+)
